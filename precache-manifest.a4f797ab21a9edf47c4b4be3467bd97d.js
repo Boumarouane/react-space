7e8b966bf7e6f496f1c51559e59886275fb53c22
Boumarouane <i.boumarouane@gmail>
2020-05-12 04:47:34 +0200

    Updates

---
+++ precache-manifest.a4f797ab21a9edf47c4b4be3467bd97d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261371f942536ba0da6709b753285207",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "433aab51efbe0c634b0c",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "61fc004fb512e159ba02",
+    "url": "/my-app/static/js/2.57f40175.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.57f40175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "433aab51efbe0c634b0c",
+    "url": "/my-app/static/js/main.ddfc7daf.chunk.js"
+  },
+  {
+    "revision": "a6fd2881bfcf7c967924",
+    "url": "/my-app/static/js/runtime-main.87f4f010.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
